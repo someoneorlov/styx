8cfd2d83d781ab1c5da779c45261ee67383a3bb5
Nikita Orlov <nikitaorlov@MacBook-Air-Nikita.local>
2023-03-05 14:56:33 +0600

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The project aims to develop a system that retrieves news articles related to a particular company, performs sentiment analysis, and generates summaries using NLP techniques. It could be useful for investors, analysts, and businesses who want to stay up-to-date on the latest news related to a company.',
+    author='Nikita Orlov',
+    license='BSD-3',
+)
