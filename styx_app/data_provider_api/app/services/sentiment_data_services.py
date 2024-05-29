6123f993b4f05ffc6b4ca42080a7b7b53edb6ca8
someone_orlov <someoneorlov@gmail.com>
2024-05-29 18:22:34 +0300

    1. Modified data_provider_api sentiment services 2. Developed ECS taks script and task defenition JSON

--- styx_app/data_provider_api/app/services/sentiment_data_services.py
+++ styx_app/data_provider_api/app/services/sentiment_data_services.py
@@ -1,3 +1,4 @@
+from typing import List
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import SQLAlchemyError, IntegrityError
 from styx_packages.data_connector.db_models import (
@@ -12,30 +13,41 @@ from ..models import (
 logger = setup_logger(__name__)
 
 
-def mark_news_as_processed(db, news_ids):
+def mark_news_as_processed(db: Session, news_ids: List[int]) -> list:
     successfully_marked_ids = []
     try:
         logger.info(f"Marking {len(news_ids)} news items as processed...")
         for news_id in news_ids:
+            existing_entry = (
+                db.query(RawNewsArticle).filter(RawNewsArticle.id == news_id).first()
+            )
+            if existing_entry and existing_entry.is_processed_sentiment:
+                logger.info(f"News item with ID {news_id} already marked as processed.")
+                successfully_marked_ids.append(news_id)
+                continue
+
             try:
                 db.query(RawNewsArticle).filter(RawNewsArticle.id == news_id).update(
                     {RawNewsArticle.is_processed_sentiment: True},
                     synchronize_session="fetch",
                 )
-                db.commit()
                 successfully_marked_ids.append(news_id)
                 logger.info(
                     f"News item with ID {news_id} marked as processed successfully."
                 )
             except SQLAlchemyError as e:
-                db.rollback()
                 logger.error(
                     f"Failed to mark news item with ID {news_id} as processed: {e}"
                 )
+        # Commit once after all entries have been processed
+        db.commit()
         logger.info(
             f"Successfully marked {len(successfully_marked_ids)} "
             "news items as processed."
         )
+    except SQLAlchemyError as e:
+        db.rollback()
+        logger.error(f"Failed to mark news items as processed, rolling back: {e}")
     except Exception as e:
         db.rollback()
         logger.error(
