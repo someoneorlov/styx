de8aaef571a572ac0b90fa4ea88bd0dec32f17cb
someone_orlov <someoneorlov@gmail.com>
2024-07-06 03:39:13 +0300

    Transitions to branches and environments

--- styx_packages/data_connector/dependencies.py
+++ styx_packages/data_connector/dependencies.py
@@ -1,8 +1,9 @@
 from .db_connector import session_factory, get_engine
 
 
-def get_db_session():
-    SessionLocal = session_factory(get_engine())
+def get_db_session(db_secret_name=None):
+    engine = get_engine(db_secret_name=db_secret_name)
+    SessionLocal = session_factory(engine)
     db = SessionLocal()
     try:
         yield db
